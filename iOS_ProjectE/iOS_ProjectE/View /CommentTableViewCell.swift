4e94028384d3b7127e7716f0e05c3705cf4eeb69
thoonk <dkrmrm878@gmail.com>
2020-10-05 17:55:23 +0900

    Add CommentTableViewCell

--- iOS_ProjectE/iOS_ProjectE/View /CommentTableViewCell.swift
+++ iOS_ProjectE/iOS_ProjectE/View /CommentTableViewCell.swift
@@ -9,15 +9,8 @@ import UIKit
 
 class CommentTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var writerImageView: UIImageView!
+    @IBOutlet weak var writerLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var reviewLabel: UILabel!
 }
