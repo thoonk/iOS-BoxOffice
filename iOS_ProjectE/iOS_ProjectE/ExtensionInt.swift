0cd4d80bf1d52aa6691e633290b5b250bb243a3d
thoonk <dkrmrm878@gmail.com>
2020-10-06 17:44:34 +0900

    Fix remark

--- iOS_ProjectE/iOS_ProjectE/ExtensionInt.swift
+++ iOS_ProjectE/iOS_ProjectE/ExtensionInt.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Int {
+    // Int 타입 변수를 문자열로 변환하고 천단위로 ','를 삽입함
+    func toStringWithComma() -> String? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        if let str = numberFormatter.string(from: NSNumber(integerLiteral: self)) {
+            return str
+        }
+        return nil
+    }
+}
