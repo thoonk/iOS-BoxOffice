03752da80a38a0dd38cce3597b37b8c3de716bdb
thoonk <dkrmrm878@gmail.com>
2020-10-07 19:58:02 +0900

    Add requestMovie for Detail

--- iOS_ProjectE/iOS_ProjectE/Model/Request.swift
+++ iOS_ProjectE/iOS_ProjectE/Model/Request.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 let DidRecieveMoviesNotification: Notification.Name = Notification.Name("DidRecieveMovies")
+let DidRecieveMovieNotification: Notification.Name = Notification.Name("DidRecieveMovie")
 
 // MARK: - 영화 목록 요청
 func requestMovies(orderType: Int){
@@ -36,3 +37,27 @@ func requestMovies(orderType: Int){
     dataTask.resume()
 }
 
+func requestMovieDetail(_ id: String){
+    guard let url: URL = URL(string: "http://connect-boxoffice.run.goorm.io/movie?id=\(id)") else{
+        print("url error")
+        return
+    }
+    
+    let session: URLSession = URLSession(configuration: .default)
+    let dataTask: URLSessionDataTask = session.dataTask(with: url) {(data: Data?, response: URLResponse?, error: Error?) in
+        if let error = error{
+            print(error.localizedDescription)
+            return
+        }
+        
+        guard let data = data else {return}
+        
+        do {
+            let apiResponse = try JSONDecoder().decode(Movie.self, from: data)
+            NotificationCenter.default.post(name: DidRecieveMovieNotification, object: nil, userInfo: ["movie": apiResponse])
+        } catch(let err){
+            print(err.localizedDescription)
+        }
+    }
+    dataTask.resume()
+}
