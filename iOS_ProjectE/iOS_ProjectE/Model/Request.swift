2f5d1bf970dacf8faee42b9a88c30d548f95cbdd
thoonk <dkrmrm878@gmail.com>
2020-10-09 20:47:06 +0900

    Add DataModel & request for Comment

--- iOS_ProjectE/iOS_ProjectE/Model/Request.swift
+++ iOS_ProjectE/iOS_ProjectE/Model/Request.swift
@@ -9,6 +9,7 @@ import Foundation
 
 let DidRecieveMoviesNotification: Notification.Name = Notification.Name("DidRecieveMovies")
 let DidRecieveMovieNotification: Notification.Name = Notification.Name("DidRecieveMovie")
+let DidRecieveCommentsNotification: Notification.Name = Notification.Name("DidRecieveComments")
 
 // MARK: - 영화 목록 요청
 func requestMovies(orderType: Int){
@@ -56,6 +57,34 @@ func requestMovieDetail(_ id: String){
             let apiResponse = try JSONDecoder().decode(Movie.self, from: data)
             NotificationCenter.default.post(name: DidRecieveMovieNotification, object: nil, userInfo: ["movie": apiResponse])
         } catch(let err){
+            print(err.localizedDescription)
+        }
+    }
+    dataTask.resume()
+}
+
+func requestMovieComments(_ id: String){
+    guard let url: URL = URL(string: "http://connect-boxoffice.run.goorm.io/comments/movie_id=\(id)") else{
+        print("url error")
+        return
+    }
+    
+    let session: URLSession = URLSession(configuration: .default)
+    let dataTask: URLSessionDataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+        if let error = error{
+            print("request Error")
+            print(error.localizedDescription)
+            return
+        }
+
+        guard let data = data else {return}
+
+        do{
+            let apiResponse: APIResponseForComments = try JSONDecoder().decode(APIResponseForComments.self, from: data)
+
+            NotificationCenter.default.post(name: DidRecieveCommentsNotification, object: nil, userInfo: ["comments": apiResponse.comments])
+
+        }catch(let err){
             print(err.localizedDescription)
         }
     }
