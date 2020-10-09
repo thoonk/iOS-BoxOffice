d65a2dd6270eafb26c571c6644ad3482ef2acd3e
thoonk <dkrmrm878@gmail.com>
2020-10-09 20:50:06 +0900

    Add synopsis & directorActorTableViewCell, cell's header

--- iOS_ProjectE/iOS_ProjectE/ViewController/DetailViewController.swift
+++ iOS_ProjectE/iOS_ProjectE/ViewController/DetailViewController.swift
@@ -13,7 +13,8 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
     
     var movie: Movie?
     var movies: Movies?
-    
+    var comments: [Comment?] = []
+
     struct TableViewSection{
         static let info = 0
         static let synopsis = 1
@@ -25,25 +26,26 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         static let synopsisCell = "synopsisCell"
         static let directorActorCell = "directorActorCell"
         static let commentCell = "commentCell"
+        static let headerCell = "headerCell"
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        guard let movies = movies else{
-            return
-        }
-        requestMovieDetail(movies.id)
+        request()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.navigationItem.title = self.movies?.title
+        
         // Do any additional setup after loading the view.
-        NotificationCenter.default.addObserver(self, selector: #selector(self.didReceiveMovieNotification(_:)), name: DidRecieveMovieNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didRecieveMovieNotification(_:)), name: DidRecieveMovieNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.didRecieveCommentsNotification(_:)), name: DidRecieveCommentsNotification, object: nil)
     }
     
-    @objc func didReceiveMovieNotification(_ noti: Notification){
+    @objc func didRecieveMovieNotification(_ noti: Notification){
         guard let movie: Movie = noti.userInfo?["movie"] as? Movie else {
             print("noti error")
             return}
@@ -53,6 +55,34 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         DispatchQueue.main.async {
             self.tableView.reloadData()
         }
+    }
+    
+    @objc func didRecieveCommentsNotification(_ noti: Notification){
+        guard let comments: [Comment] = noti.userInfo?["comments"] as? [Comment] else{
+            print("comment error")
+            return
+        }
+        self.comments = comments
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    private func request(){
+        guard let movies = movies else{
+            return
+        }
+        
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        requestMovieDetail(movies.id)
+        dispatchGroup.leave()
+        
+        dispatchGroup.enter()
+        requestMovieComments(movies.id)
+        dispatchGroup.leave()
     }
     
     // MARK: - setGrandImageView
@@ -69,27 +99,35 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     // MARK: - tableView
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 4
     }
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == TableViewSection.comment ? comments.count : 1
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.section == TableViewSection.info ? 280 : UITableView.automaticDimension
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return section == TableViewSection.info ? CGFloat.leastNormalMagnitude : 50
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return .leastNormalMagnitude
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let movie = movie else{
             return UITableViewCell()
         }
         switch indexPath.section{
+        // 정보
         case TableViewSection.info:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.infoCell, for: indexPath) as? InfoTableViewCell else {
                 return UITableViewCell()
                 
             }
-            cell.titleLabel.text = movie.title
-            cell.dateLabel.text = movie.date
-            cell.genreDurationLabel.text = "\(movie.genre)/\(movie.duration)분"
-            cell.reservationLabel.text = "\(movie.reservationGrade)위 \(movie.reservationRate)%"
-            cell.userRatingLabel.text = "\(movie.userRating)"
-            cell.audienceLabel.text = movie.audience.toStringWithComma() ?? "\(movie.audience)"
+            cell.mappingData(movie)
             setGradeImageView(cell.gradeImageView, grade: movie.grade)
+            
             
             DispatchQueue.global(qos: .background).async {
 
@@ -115,10 +153,63 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
             }
             
             return cell
+        // 줄거리
+        case TableViewSection.synopsis:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.synopsisCell, for: indexPath) as? SynopsisTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            cell.synopsisLabel.text = movie.synopsis
+            return cell
+        // 감독/연출
+        case TableViewSection.directorActor:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.directorActorCell, for: indexPath) as? DirectorActorTableViewCell else{
+                return UITableViewCell()
+            }
+            
+            cell.directorLabel.text = movie.director
+            cell.actorLabel.text = movie.actor
+            
+            return cell
+        // 한줄평
+        case TableViewSection.comment:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.commentCell, for: indexPath) as? CommentTableViewCell, let comment = self.comments[indexPath.row] else{
+                print("no commentCell")
+                return UITableViewCell()
+            }
+            
+            cell.mappingData(comment)
+            
+            return cell
             
         default:
             return UITableViewCell()
         }
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 50))
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: 200, height: 50))
+        let button = UIButton(frame: CGRect(x: tableView.frame.size.width - 30, y: 20, width: 20, height: 20))
+        
+        switch section{
+        case TableViewSection.synopsis:
+            label.text = "줄거리"
+            button.isHidden = true
+        case TableViewSection.directorActor:
+            label.text = "감독/출연"
+            button.isHidden = true
+        case TableViewSection.comment:
+            label.text = "한줄평"
+            button.setImage(UIImage(named: "btn_compose"), for: .normal)
+            button.isHidden = false
+        
+        default:
+            return nil
+        }
+        view.addSubview(label)
+        view.addSubview(button)
+        
+        return view
     }
 }
